20209cdc955006f6ab9718f75b3ec1bf81d43446
jairomqcode <112170663+jairomqcode@users.noreply.github.com>
2022-11-12 17:37:44 -0500

    Delete Programming for Everybody (Getting Started with Python) directory

--- Programming for Everybody (Getting Started with Python)/Semana 1/tarea4.py
+++
@@ -1,9 +0,0 @@
-"""
-Escribe un programa que le pida al usuario una temperatura en grados Celcius, la convierta a grados Fahrenheit e imprima por pantalla la temperatura convertida.
-"""
-
-temp_celcius = float(input("Introduce la temperatura en celcius: "))
-
-temp_fahrenheit = (temp_celcius * 1.8) + 32
-
-print(temp_fahrenheit)
