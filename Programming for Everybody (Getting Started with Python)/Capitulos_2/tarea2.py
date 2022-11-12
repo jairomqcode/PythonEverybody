2495662c9142ba6b513e3e1e1b57c7173672783f
jairomqcode <112170663+jairomqcode@users.noreply.github.com>
2022-11-12 23:05:02 +0000

    Add files via upload

---
+++ Programming for Everybody (Getting Started with Python)/Capitulos_2/tarea2.py
@@ -0,0 +1,9 @@
+"""
+Escribe un programa para pedirle al usuario el número de horas y la tarifa por hora para calcular el salario bruto.
+"""
+horas_trabajadas = float(input("Introduce la cantidad de horas trabajadas: "))
+tarifa_horas = float(input("Introduce la tarifa por hora: "))
+
+salario_bruto = horas_trabajadas * tarifa_horas
+
+print(salario_bruto)
---
+++ Programming for Everybody (Getting Started with Python)/Capitulos_2/tarea3.py
@@ -0,0 +1,19 @@
+""" 
+Asume que ejecutamos las siguientes sentencias de asignación: 
+
+ancho = 17
+alto = 12.0
+
+1. ancho / 2
+2. ancho / 2.0
+3. alto / 3
+4. 1 + 2 * 5
+"""
+
+ancho = 17
+alto = 12.0
+
+print(ancho/2) 
+print(ancho/2.0)
+print(alto/3)
+print(1 + 2 * 5)
