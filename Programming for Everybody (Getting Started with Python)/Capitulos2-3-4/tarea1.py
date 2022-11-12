9f8d702aa6c49cfa9a2a872178e17a4c22f0b01e
jairomqcode <112170663+jairomqcode@users.noreply.github.com>
2022-11-12 17:48:31 -0500

    Delete Programming for Everybody (Getting Started with Python)/Capitulos2-3-4 directory

--- Programming for Everybody (Getting Started with Python)/Capitulos2-3-4/tarea1.py
+++
@@ -1,7 +0,0 @@
-"""
-Escribe un programa que use 'input' para pedirle al usuario su nombre y luego darle la bienvenida.
-"""
-
-nombre = input("Introduce tu nombre: ")
-
-print(f"Hola {nombre}, bienvenido.")
