15907c28545abdb6a095ffbc98afb19bde3d5258
jairomqcode <112170663+jairomqcode@users.noreply.github.com>
2022-11-13 20:43:39 -0500

    Cálculo de salario aplicando condicionales

--- Programming for Everybody (Getting Started with Python)/Capitulos_2/tarea3.py
+++ Programming for Everybody (Getting Started with Python)/Capitulos_2/tarea3.py
@@ -1,20 +1,35 @@
 """ 
-Asume que ejecutamos las siguientes sentencias de asignación: 
 
-ancho = 17
-alto = 12.0
+Escriba un programa para solicitar al usuario las horas y la tarifa por hora utilizando la entrada para calcular el pago bruto. 
+Pague la tarifa por hora por las horas hasta 40 y 1,5 veces la tarifa por hora por todas las horas trabajadas por encima de las 40 horas. 
+Utilice 45 horas y una tarifa de 10,50 por hora para probar el programa (la paga debería ser de 498,75). Debe usar input para leer una 
+cadena y float() para convertir la cadena en un número. No se preocupe por el error al verificar la entrada del usuario; suponga que el 
+usuario escribe los números correctamente.
 
-1. ancho / 2
-2. ancho / 2.0
-3. alto / 3
-4. 1 + 2 * 5
 """
 
-ancho = 17
-alto = 12.0
+# Datos de entrada por parte del usuario:
+horas = input("Enter Hours:")
+tarifa = input ("Enter rate: ")
 
-print(ancho/2) 
-print(ancho/2.0)
-print(alto/3)
-print(1 + 2 * 5)
+# Convertir los datos de tipo string a float:
+h = float(horas)
+t = float(tarifa)
+
+# Calculamos el valor del pago total para las 40 horas trabajadas:
+# También tomamos en cuenta el valor de la tarifa por hora en 10.50
+sueldo_mensual = 40 * t
+
+# Calculamos el número de horas extras trabajada:
+h_extra = h - 40
+
+# Calculamos la tarifa por hora extras:
+tarifa_horas_extra = t * 1.5
+
+# Aplicamos nuestras condicionales bien sea el caso:
+if h > 40:
+    sueldo_mensual_extra = sueldo_mensual + ((h_extra)*(tarifa_horas_extra))
+    print(sueldo_mensual_extra)
+else:
+    print(sueldo_mensual)
 
