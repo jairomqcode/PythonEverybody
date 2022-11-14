343a6d1b643c7490d6751d69cdcd8365056d5fd6
jairomqcode <112170663+jairomqcode@users.noreply.github.com>
2022-11-13 21:03:55 -0500

    Cálculo de salario (Creando una función).

--- Programming for Everybody (Getting Started with Python)/Capitulos_2/tarea8.py
+++ Programming for Everybody (Getting Started with Python)/Capitulos_2/tarea8.py
@@ -1,18 +1,32 @@
 """
 
-Reescribe el programa del cálculo del salario para darle al empleado 1.5 veces
-la tarifa horaria para todas las horas trabajadas que excedan de 40.
+Escriba un programa para solicitar al usuario las horas y la tarifa por hora utilizando la entrada para calcular el pago bruto. 
+El pago debe ser la tarifa normal por horas hasta 40, y tiempo y medio por la tarifa por hora para todas las horas trabajadas por 
+encima de las 40 horas. Ponga la lógica para realizar el cálculo del pago en una función llamada computepay() y use la función para 
+realizar el cálculo. La función debe devolver un valor. Utilice 45 horas y una tarifa de 10,50 por hora para probar el programa 
+(la paga debería ser de 498,75). Debe usar input para leer una cadena y float() para convertir la cadena en un número. No se preocupe
+por el error al verificar la entrada del usuario a menos que lo desee; puede asumir que el usuario escribe los números correctamente. 
+No nombre su variable sum ni use la función sum().
 
 """
-# Introducimos los valores:
-horas_trabajadas = float(input("Introduce la cantidad de horas trabajadas: "))
-tarifa_horas = float(input("Introduce la tarifa por hora: "))
-horas_extras = float(input("Introduce las horas extras trabajadas: "))
 
-# Ecuaciones para los cálculos:
-salario_bruto = horas_trabajadas * tarifa_horas
-salario_extra = horas_extras * (tarifa_horas * 1.5)
-salario_total = salario_bruto + salario_extra
+# Definimos la función parametrizada en función a las horas trabajadas y la tarifa por hora:
+def computepay(horas, tarifas):
+    #Nuestra condicionales:
+    if horas < 40 :
 
-# Resultado final:
-print(salario_total)
+        return horas * tarifas
+
+    else :
+        return ((40 * tarifas) + ((horas - 40) * (tarifas * 1.5)))
+
+# Datos de entradas
+horas = input("Introduce el numero de horas:")
+tarifas = input("Introduce la tarifa por hora trabajada: ")
+
+#Convertimos los tipos de datos:
+horas2 = float(horas)
+tarifas2 = float(tarifas)
+
+#Resultado:
+print("Pago", computepay(horas2,tarifas2))
