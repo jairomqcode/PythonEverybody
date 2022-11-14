9b2ed94047bdf23d8c79538a12c601b00c091e79
jairomqcode <112170663+jairomqcode@users.noreply.github.com>
2022-11-13 20:35:27 -0500

    Solicitud de información.

--- Programming for Everybody (Getting Started with Python)/Capitulos_2/tarea1.py
+++ Programming for Everybody (Getting Started with Python)/Capitulos_2/tarea1.py
@@ -1,5 +1,9 @@
 """
-Escribe un programa que use 'input' para pedirle al usuario su nombre y luego darle la bienvenida.
+
+Escriba un programa que utilice la entrada para pedirle a un usuario su nombre y luego le dé la bienvenida. 
+Tenga en cuenta que la entrada aparecerá un cuadro de diálogo. Ingrese Sarah en el cuadro emergente cuando se 
+le solicite para que su salida coincida con la salida deseada.
+
 """
 
 nombre = input("Introduce tu nombre: ")
