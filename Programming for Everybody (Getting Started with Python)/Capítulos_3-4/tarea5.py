5bb21eae0e2be728c6e9653e759d18dfe4cd17d9
jairomqcode <112170663+jairomqcode@users.noreply.github.com>
2022-11-13 21:19:10 -0500

    Cálculo de salario definiendo una función

--- Programming for Everybody (Getting Started with Python)/Capítulos_3-4/tarea5.py
+++ Programming for Everybody (Getting Started with Python)/Capítulos_3-4/tarea5.py
@@ -1,16 +1,17 @@
 """
 
-Reescribe el programa de cálculo del salario, con tarifa y media para las horas extras (1.5), 
-y crea una función llamada calculo_salario que reciba dos parámetros (horas y tarifa). 
-
-Introduzca Horas: 45
-Introduzca Tarifa: 10
-Salario: 475.0
+Escriba un programa para solicitar al usuario las horas y la tarifa por hora utilizando la entrada para calcular el pago bruto. 
+El pago debe ser la tarifa normal por horas hasta 40, y tiempo y medio por la tarifa por hora para todas las horas trabajadas por 
+encima de las 40 horas. Ponga la lógica para realizar el cálculo del pago en una función llamada computepay() y use la función para 
+realizar el cálculo. La función debe devolver un valor. Utilice 45 horas y una tarifa de 10,50 por hora para probar el programa 
+(la paga debería ser de 498,75). Debe usar input para leer una cadena y float() para convertir la cadena en un número. No se preocupe
+por el error al verificar la entrada del usuario a menos que lo desee; puede asumir que el usuario escribe los números correctamente. 
+No nombre su variable sum ni use la función sum().
 
 """
 
 # Definimos la función parametrizada en función a las horas trabajadas y la tarifa por hora:
-def salario_total(horas, tarifas):
+def computepay(horas, tarifas):
     #Nuestra condicionales:
     if horas < 40 :
 
@@ -28,4 +29,4 @@ horas2 = float(horas)
 tarifas2 = float(tarifas)
 
 #Resultado:
-print("Pago", salario_total(horas2,tarifas2))
+print("Pago", computepay(horas2,tarifas2))
