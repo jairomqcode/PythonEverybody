344eab64f7aa24b3afad6ee3aaccbb32126b9a35
jairomqcode <112170663+jairomqcode@users.noreply.github.com>
2022-11-16 19:54:32 -0500

    Update proyecto3.py

--- Programming for Everybody (Getting Started with Python)/Capítulo3/proyecto3.py
+++ Programming for Everybody (Getting Started with Python)/Capítulo3/proyecto3.py
@@ -1,3 +1,33 @@
+"""
+
+Escribe un programa que solicite una puntuación entre 0.0 y 1.0. Si la puntuación está fuera de ese rango, muestra un mensaje de error. 
+Si la puntuación está entre 0.0 y 1.0, muestra la calificación usando la tabla siguiente:
+
+Puntuación Calificación
+>= 0.9 Sobresaliente
+>= 0.8 Notable
+>= 0.7 Bien
+>= 0.6 Suficiente
+< 0.6 Insuficiente
+
+
+Introduzca puntuación: 0.95
+Sobresaliente
+
+Introduzca puntuación: perfecto
+Puntuación incorrecta
+
+Introduzca puntuación: 10.0
+Puntuación incorrecta
+
+Introduzca puntuación: 0.75
+Bien
+
+Introduzca puntuación: 0.5
+Insuficiente
+
+"""
+
 score = input("Enter Score: ")
 
 #Convertimos el dato de entrada de String a Float y lo guardamos en una variable:
