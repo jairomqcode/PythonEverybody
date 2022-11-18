988495034090288d13b823d67ec70455292e2964
jairomqcode <112170663+jairomqcode@users.noreply.github.com>
2022-11-18 14:09:17 +0000

    Add files via upload

---
+++ Programming for Everybody (Getting Started with Python)/Capítulo2/tarea5.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio:
+    - Crear variables una "pais" y otra "continente".
+    - Mostrar su valor por pantalla (imprimir).
+    - Poner un comentario diciendo el tipo de dato.
+"""
+
+pais = "España"         #Tipo de dato: string.
+continente = "Europa"   #Tipo de dato: string.
+
+print(f"{pais} - {continente}")
